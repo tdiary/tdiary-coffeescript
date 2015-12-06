85fd6aace129176a43affdfb2a79c3f8672883be
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-12-06 11:18:26 +0900

    fixed wrong usage

--- lib/tdiary-coffeescript.rb
+++ lib/tdiary-coffeescript.rb
@@ -2,4 +2,4 @@ require 'tdiary'
 require 'tdiary/rack/assets/precompile'
 load 'tdiary/tasks/assets/compile.rake'
 
-TDiary::Application::configuration.assets_precompile = true
+TDiary::Application.config.assets_precompile = true
